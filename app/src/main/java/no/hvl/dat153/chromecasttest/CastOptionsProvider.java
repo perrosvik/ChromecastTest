008ae56e4cf87874a048fb6244148313326642cc
Per Magne <per.magne.rosvik@gmail.com>
2018-02-19 16:29:45 +0100

    Code cleanup

--- app/src/main/java/no/hvl/dat153/chromecasttest/CastOptionsProvider.java
+++ app/src/main/java/no/hvl/dat153/chromecasttest/CastOptionsProvider.java
@@ -17,21 +17,13 @@ import java.util.List;
  */
 
 public class CastOptionsProvider implements OptionsProvider {
-
-    public static final String CUSTOM_NAMESPACE = "urn:x-cast:custom_namespace";
-
     @Override
     public CastOptions getCastOptions(Context context) {
-
-        List<String> supportedNamespaces = new ArrayList<>();
-        supportedNamespaces.add(CUSTOM_NAMESPACE);
-
         CastOptions castOptions = new CastOptions.Builder()
                 .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
                 .build();
         return castOptions;
     }
-
     @Override
     public List<SessionProvider> getAdditionalSessionProviders(Context context) {
         return null;
